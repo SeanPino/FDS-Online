7a31dbecd35a402e044751351d225ef9900b04c0
Sean Pino <seanpino@gmail.com>
2015-09-16 10:54:15 -0500

    Added The table creation and insert sql commands for the tables

---
+++ DatabaseInsertStatements.sql
@@ -0,0 +1,3 @@
+INSERT INTO Statuses VALUES (1,'Queued','The job has been added to the queue')
+INSERT INTO Statuses VALUES (2,'Processing','The job is the currently being processed')
+INSERT INTO Statuses VALUES (3,'Complete','The job has been successfully run and is completed')
