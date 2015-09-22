ca81841e97ea18ca046c642a1302a0e4a10426ac
Alex Neises <alex@reflectivegroup.com>
2015-09-21 20:15:36 -0500

    Added main page for production site.

--- templates/home.php
+++ templates/home.php
@@ -1,13 +1,48 @@
 <html>
 	<head>
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="/foundation/bower_components/foundation/css/foundation.min.css?<?php print time(); ?>" />
 		<link rel="stylesheet" href="/foundation/stylesheets/app.css?<?php print time(); ?>" />
 		<script src="/foundation/bower_components/foundation/js/vendor/modernizr.js?<?php print time(); ?>"></script>
 	</head>
 	<body>
 		<div class="row">
-			<div class="small-12 columns">
-				<h1>Pyro Simulation</h1>
+			<div id="main" class="small-12 columns">
+				<div class="row">
+					<img src="/images/100px_PyroSim_Logo.png"/>
+					<div class="small-12 columns">
+						<h1>Online Simulator</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="small-12 columns">
+						<p>Select a file to upload.  The server will run a simulation and you will be updated with its status.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="small-12 columns">
+						<form>
+							<fieldset>
+								<legend>FDS Upload</legend>
+								<input type="file" name="file" />
+							</fieldset>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div id="footer" class="small-12 columns">
+				<div class="row">
+					<div class="small-12 columns">
+						<p>Built with love by Alex Neises, Sean Pino, &amp; Shawn Contant.</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="small-12 columns">
+						<p>Version 0.1.0</p>
+					</div>
+				</div>
 			</div>
 		</div>
 
