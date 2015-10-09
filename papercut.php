eab8b369cca2c384348f802a282652e844bab8c7
Sean Pino <seanpino@gmail.com>
2015-10-09 10:35:20 -0500

    papercut working for most cases

--- papercut.php
+++ papercut.php
@@ -14,47 +14,115 @@ class pc
 	public static function getTime($filename)
 	{
 
-		//$strings = file_get_contents($filename);
+
 		$file = pc::createTempFile($filename);
 		$lineCount = count($file);
-		$firstTime = $file[33];
+		$firstTime = pc::searchLine($file,$lineCount,'F');
+		$lastTime = pc::searchLine($file, $lineCount, 'B');
 		//var_dump($firstTime);
-		//$firstTimeA = explode( " ", $firstTime);
-		$cuttingSpaces = preg_split('/[\s]+/', $firstTime);
-		$firstTime = $cuttingSpaces[5];
-		//var_dump($segments);
-		//$replaced = preg_replace('/\D/', '', $firstTime);
-		//var_dump($firstTimeA);
-		//var_dump($replaced);
+		//var_dump($lastTime);
 
-		for($i = $lineCount-10; $i < $lineCount-1; $i++)
+		if($firstTime <= 0 || $firstTime == null || $lastTime == null)
 		{
-			if( strpos($file[$i],"Total time:") != false)
-			{
-				$line = $i; 
-				break;
-			}
+			$error = "Divided by zero";
+			return $error;
+			//return json_encode($error);
 		}
-		$lastTime = $file[$line];
-
-		//print $firstTime . "\n";
-		$lastTime = preg_split('/[\s]+/', $lastTime);
-		$lastTime = $lastTime[7];
-		//print $lastTime . "\n";
-
 		$percentage = ($lastTime/$firstTime) * 100 ;
-		print round($percentage, 2) . "%";
+		return round($percentage, 2) . "%\n";
 
 	}
 
+	public static function searchLine($file, $lineCount, $direction)
+	{
+		$lineNumber = null;
+		if(($direction == 'F'))
+		{
+			$lineNumber = pc::getLineNumber($file, "Simulation End Time (s)", 0, 50, $lineCount, $direction);
+		}
+		else if($direction == 'B')
+		{
+			$lineNumber = pc::getLineNumber($file, "Total time:",$lineCount-10, $lineCount-1, $lineCount, $direction);
+		}
+		//var_dump($lineNumber);
+		if($lineNumber != null)
+		{
+			if($direction == 'F')
+			{
+				//var_dump($file[$lineNumber]);	
+				$time = pc::getNumber($file[$lineNumber], 1);
+			}
+			else if($direction == 'B')
+			{
+				$time = pc::getNumber($file[$lineNumber], 2);
+			}
+		}
+		else
+		{
+			$time = null;
+		}
+		return $time;
 
+	}
+
+	public static function getLineNumber($file, $searchString,$startpos, $endpos, $lineCount, $direction)
+	{
+		$line = -1;
+		$newStartPos = $startpos;
+		$newEndPos =$endpos;
+
+		while($line == -1 || $newEndPos <=10 || $newStartPos >= $lineCount )
+		{
+			for($i = $newStartPos; $i < $newEndPos; $i++)
+			{
+				if(strpos($file[$i], $searchString) != false)
+					{
+						$line = $i; 
+						return $line;
+					}			
+			}
+
+			//set new positions for next iteration if needed
+			if($direction == 'F')
+			{
+				$newStartPos = $newStartPos + 50;
+				$newEndPos = $newStartPos +50;
+			}
+			else if($direction == 'B')
+			{
+				$newStartPos = $newStartPos - 10;
+				$newEndPos = $newStartPos + 10;
+			}
+		}
+		return $line;
+	}
+
+	public static function getNumber($line, $which)
+	{
+		$number = null;
+		$line = preg_split('/[\s]+/', $line);
+		$lineNum = 0;
+		foreach($line as $index)
+		{
+			if (strpos($index,'.') != false) 
+			{
+				if($which == 2)
+				{	
+					$which = $which-1;
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			$lineNum++;
+		}
+
+		$number = $line[$lineNum];
+		return $number;
+	}
 }
-
-
-
-
-
-
 
 
 
