696e26f22811bc87ecebe6f2f5e8d6d6bca341b4
Shawnco <shawnc366@gmail.com>
2015-11-02 09:57:35 -0600

    Worked on api/v1/download, also added API comments

--- api.php
+++ api.php
@@ -69,14 +69,38 @@ $app->delete('/api/v1/delete/:id', function($id) use($app){
  *      curl 'http://pyro.demo/api/v1/download/1'
  */
 $app->get('/api/v1/download/:id', function($id) use($app){
-	print $id;
+	//print $id;
 	
         // Get the job to be downloaded.
-        $job = DB::FindJob($id, $app);
-        $completed = "/uploads/completed" . $job["timestamp"];
-        
-        // Create the zip.
-        $zip = new ZipArchive();
+//        $job = DB::FindJob($id, $app);
+//        echo $id . "<br />";
+//        $job["timestamp"] = 1446044514;
+//        // Create the zip.
+//        $zip = new ZipArchive();
+//        echo "uploads/" . $job["timestamp"] . "/" . $job["timestamp"] . ".zip<br />";
+//        // Add everything except the original FDS file to the list of stuff to zip.
+//        $zip->open("uploads/" . $job["timestamp"] . "/" . $job["timestamp"] . ".zip", ZipArchive::CREATE);
+//        $files = scandir(".");
+//        foreach($files as $f){
+//            echo $f;
+//            if(substr($f, -4) !== ".fds"){  // Don't include the original FDS file.
+//                $zip->addFile($f);
+//            }
+//        }
+//        $zip->close();
+//        
+//        // Now make it available for download.
+//        return $zip;
+    chmod(".", 0777);
+    $zip = new ZipArchive();
+    $files = scandir(".");
+    $zip->open("zip.zip", ZipArchive::CREATE);
+    var_dump($zip);
+    foreach($files as $f){
+        $zip->addFile($f);
+    }
+    $res = $zip->close();
+    var_dump($res);
 });
 
 /**
