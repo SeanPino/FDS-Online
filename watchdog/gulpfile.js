fc4df0b127e18932613d58b0b7c5a84bd73aab56
Joe Bennett <jebennett099@gmail.com>
2016-03-24 17:44:15 -0500

    gulp-watch handler, log any newly added FDS files

--- watchdog/gulpfile.js
+++ watchdog/gulpfile.js
@@ -1,25 +1,25 @@
-var gulp = require('gulp');
-var exec = require('exec');
+let gulp = require('gulp'),
+    exec = require('exec'),
+    watch = require('gulp-watch');
 
-var paths = {
+let ADD = 'add';
+
+let paths = {
   jobs: ['../uploads/**/*.fds'],
   parcel: ['../parcel.php']
 };
 
-var runCommand = "php -r 'require \"" + paths.parcel + "\"; P::run();'";
+//var runCommand = "php -r 'require \"" + paths.parcel + "\"; P::run();'";
 
 gulp.task('jobs', function () {
-  exec([runCommand], function(err, out, code) {
-    if (err instanceof Error)
-      throw err;
-    process.stderr.write(err);
-    process.stdout.write(out);
-    process.exit(code);
-  });
+
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.jobs, ['jobs']);
+  watch(paths.jobs, function (vinyl) {
+    if (vinyl.event == ADD)
+      console.log("Houston, we have a new FDS job");
+  });
 });
 
-gulp.task('default', ['watch', 'jobs']);
+gulp.task('default', ['watch']);
